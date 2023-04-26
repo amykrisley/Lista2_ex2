274b35ba55d5b7e102a8b385680066d6f152dd45
amykrisley <130614797+amykrisley@users.noreply.github.com>
2023-04-26 20:18:03 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2_ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            Console.WriteLine("Informe o primeiro valor:");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o segundo valor:");
+            b = int.Parse(Console.ReadLine());
+            if (a > b)
+                Console.WriteLine("o maior valor é: {0}", a);
+            else if (a < b)
+                Console.WriteLine("o maior valor é: {0}", b);
+            else
+                Console.WriteLine("os valores são iguais");
+        }
+    }
+}
